e848af654db9b844c3d01ac9bcfc305278a124c1
gunabam <gunabam@gmail.com>
2025-04-24 10:17:41 -0400

    added prep dataset functions for bloom mol training

---
+++ omnicons/datasetprep/__init__.py
@@ -0,0 +1,39 @@
+import os
+import pickle
+from glob import glob
+
+import pandas as pd
+from tqdm import tqdm
+
+from omnicons import dataset_dir
+
+
+def prepare_molecular_graphs(
+    dataset_fp: str = f"{dataset_dir}/molecule_training_data.csv",
+    bloom_dos_dir: str = f"{dataset_dir}/bloom_dos_annotations",
+    output_dir: str = f"{dataset_dir}/molecular_graphs",
+    cpus: int = 10,
+):
+    from Bloom import BloomDOS
+    from Bloom.BloomEmbedder.graphs.MoleculeGraph import MoleculeGraph
+
+    # get bloom annotations
+    data = pd.read_csv(dataset_fp).to_dict("records")
+    BloomDOS.multiprocess_subission(
+        submission_list=data, output_dir=bloom_dos_dir, cpus=cpus
+    )
+
+    # get molecular graphs
+    filenames = glob(f"{bloom_dos_dir}/*.json")
+    for fp in tqdm(filenames, desc="Creating molecular graphs"):
+        metabolite_id = int(fp.split("/")[-1].split(".")[0])
+        output_fp = f"{output_dir}/{metabolite_id}.pkl"
+        if os.path.exists(output_fp):
+            continue
+        # get bloom annotations
+        out = MoleculeGraph.build_from_bloom_graph(
+            graph_id=metabolite_id,
+            bloom_graph_fp=fp,
+        )
+        G = out["graph"]
+        pickle.dump(G, open(output_fp, "wb"))
